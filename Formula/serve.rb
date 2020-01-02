346aa082558d8e7bb09ac2c40e2ef825e1a23f46
kulikov <kulikov.dm@gmail.com>
2020-01-02 12:30:23 +0300

    up

--- Formula/serve.rb
+++ Formula/serve.rb
@@ -4,8 +4,8 @@ class Serve < Formula
   version "1.6.94"
   bottle :unneeded
 
-  url "https://github.com/servehub/serve/releases/download/v1.6.94/serve-tools-v1.6.94-darwin-amd64"
-  sha256 "51bf8dc74f30ac6b277999094416e8a4fdd5bcd099e43789a28b91a7e33f71e7"
+  url "https://github.com/servehub/serve/releases/download/v1.6.94/serve-v1.6.94-darwin-amd64"
+  sha256 "173f7f8bb25c5d7973ce05f98e260b08fcd01a03f82a74aa94a4ddcc63849327"
 
   def install
     bin.install "serve"
