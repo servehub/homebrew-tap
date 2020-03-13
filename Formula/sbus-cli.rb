4692e81b9ec8a962637d1f84191b95bb51debfb0
kulikov <kulikov.dm@gmail.com>
2020-03-13 14:12:05 +0300

    sbus-cli 1.0.3

--- Formula/sbus-cli.rb
+++ Formula/sbus-cli.rb
@@ -1,14 +1,14 @@
 class SbusCli < Formula
   desc "sbus-cli"
   homepage "https://github.com/servehub/sbus-cli"
-  version "1.0.2"
+  version "1.0.3"
   bottle :unneeded
 
-  url "https://github.com/servehub/sbus-cli/releases/download/v1.0.2/sbus-v1.0.2-darwin-amd64"
-  sha256 "1abe7017db9be64f202d81cda88d96816d75953775c0144b8102e73dab179551"
+  url "https://github.com/servehub/sbus-cli/releases/download/v1.0.3/sbus-v1.0.3-darwin-amd64"
+  sha256 "08f71e6f43da993bcde74b9acf8cf562fc512240a5925b97d2fe66d3321ad9fa"
 
   def install
-    bin.install "sbus-v1.0.2-darwin-amd64" => "sbus"
+    bin.install "sbus-v1.0.3-darwin-amd64" => "sbus"
   end
 
   test do
