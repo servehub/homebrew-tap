f04d7c816ff8d671b4fba6b46a29decfef04d5c7
kulikov <kulikov.dm@gmail.com>
2021-09-03 10:50:35 +0300

    update serve and sbus-cli

--- Formula/sbus-cli.rb
+++ Formula/sbus-cli.rb
@@ -1,14 +1,14 @@
 class SbusCli < Formula
   desc "sbus-cli"
   homepage "https://github.com/servehub/sbus-cli"
-  version "1.0.4"
+  version "1.0.5"
   bottle :unneeded
 
-  url "https://github.com/servehub/sbus-cli/releases/download/v1.0.4/sbus-v1.0.4-darwin-amd64"
-  sha256 "c983cfa2acbb91ed8dfd8f515844bb6553108c4583a4f737fd518d8710e24e9c"
+  url "https://github.com/servehub/sbus-cli/releases/download/v1.0.5/sbus-v1.0.5-darwin-amd64"
+  sha256 "e6359c47c75133b67c7388909dedf42b5ca3b1cda19aca55c7d3290a1be6ac90"
 
   def install
-    bin.install "sbus-v1.0.4-darwin-amd64" => "sbus"
+    bin.install "sbus-v1.0.5-darwin-amd64" => "sbus"
   end
 
   test do
--- Formula/serve.rb
+++ Formula/serve.rb
@@ -1,14 +1,14 @@
 class Serve < Formula
   desc "serve"
   homepage "https://github.com/servehub/serve"
-  version "1.6.109"
+  version "1.6.133"
   bottle :unneeded
 
-  url "https://github.com/servehub/serve/releases/download/v1.6.109/serve-v1.6.109-darwin-amd64"
-  sha256 "eec3aff2d355f51927896efc7219adee3a3ad5c366dc4ed69a3e274e2a02619e"
+  url "https://github.com/servehub/serve/releases/download/v1.6.133/serve-v1.6.133-darwin-amd64"
+  sha256 "800093aece1d2a3444df7a0ece7773646deb5000ba81430de36594651ec04b22"
 
   def install
-    bin.install "serve-v1.6.109-darwin-amd64" => "serve"
+    bin.install "serve-v1.6.133-darwin-amd64" => "serve"
   end
 
   test do
