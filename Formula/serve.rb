f2a79a0137c2de914bb9dbec709ef1de1f236cec
Dmitry Kulikov <kulikov.dm@gmail.com>
2022-03-08 14:27:05 +0000

    Update serve.rb

--- Formula/serve.rb
+++ Formula/serve.rb
@@ -2,7 +2,6 @@ class Serve < Formula
   desc "serve"
   homepage "https://github.com/servehub/serve"
   version "1.6.133"
-  bottle :unneeded
 
   url "https://github.com/servehub/serve/releases/download/v1.6.133/serve-v1.6.133-darwin-amd64"
   sha256 "800093aece1d2a3444df7a0ece7773646deb5000ba81430de36594651ec04b22"
