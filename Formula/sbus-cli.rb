eb182d7a558e7147f3c327ab66a2ae0f36614ad3
Dmitry Kulikov <kulikov.dm@gmail.com>
2022-03-08 14:25:26 +0000

    Update sbus-cli.rb

--- Formula/sbus-cli.rb
+++ Formula/sbus-cli.rb
@@ -2,7 +2,6 @@ class SbusCli < Formula
   desc "sbus-cli"
   homepage "https://github.com/servehub/sbus-cli"
   version "1.0.5"
-  bottle :unneeded
 
   url "https://github.com/servehub/sbus-cli/releases/download/v1.0.5/sbus-v1.0.5-darwin-amd64"
   sha256 "e6359c47c75133b67c7388909dedf42b5ca3b1cda19aca55c7d3290a1be6ac90"
