7b60281517d0dbbde24f6546a08ef527f91a80f1
Dmitry Kulikov <kulikov.dm@gmail.com>
2022-05-05 19:31:48 +0100

    remove :unneeded

--- Formula/sbus-cli.rb
+++ Formula/sbus-cli.rb
@@ -2,7 +2,6 @@ class SbusCli < Formula
   desc "sbus-cli"
   homepage "https://github.com/servehub/sbus-cli"
   version "1.0.6"
-  bottle :unneeded
 
   url "https://github.com/servehub/sbus-cli/releases/download/v1.0.6/sbus-v1.0.6-darwin-amd64"
   sha256 "2da9960c394ad9765c087e40f9c1248a66c8a438f4084d4bb1fd9b810f5c01de"
--- Formula/serve.rb
+++ Formula/serve.rb
@@ -2,7 +2,6 @@ class Serve < Formula
   desc "serve"
   homepage "https://github.com/servehub/serve"
   version "1.6.146"
-  bottle :unneeded
 
   url "https://github.com/servehub/serve/releases/download/v1.6.146/serve-v1.6.146-darwin-amd64"
   sha256 "2d3325eb54bd8b632c19467c78505c2e2a59f8f7b31792973a1d0914298856e9"
