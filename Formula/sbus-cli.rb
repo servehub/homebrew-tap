171347867f1e5556518ae3fd2d3ad0b3853b6906
Dmitry Kulikov <kulikov.dm@gmail.com>
2022-05-11 16:25:31 +0100

    update sbus-cli

--- Formula/sbus-cli.rb
+++ Formula/sbus-cli.rb
@@ -1,13 +1,14 @@
 class SbusCli < Formula
   desc "sbus-cli"
   homepage "https://github.com/servehub/sbus-cli"
-  version "1.0.6"
+  version "1.0.7"
+  bottle :unneeded
 
-  url "https://github.com/servehub/sbus-cli/releases/download/v1.0.6/sbus-v1.0.6-darwin-amd64"
-  sha256 "2da9960c394ad9765c087e40f9c1248a66c8a438f4084d4bb1fd9b810f5c01de"
+  url "https://github.com/servehub/sbus-cli/releases/download/v1.0.7/sbus-v1.0.7-darwin-amd64"
+  sha256 "bc6a08aa44ba9eba643ad6176f1f4df37c15673b25e9ceb85d009b33cc39d56f"
 
   def install
-    bin.install "sbus-v1.0.6-darwin-amd64" => "sbus"
+    bin.install "sbus-v1.0.7-darwin-amd64" => "sbus"
   end
 
   test do
