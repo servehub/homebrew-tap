0b7db67aad0ea0decdcb207d3139faf561b9d2da
Dmitry Kulikov <kulikov.dm@gmail.com>
2022-07-13 18:08:47 +0100

    update serve

--- Formula/serve.rb
+++ Formula/serve.rb
@@ -1,13 +1,13 @@
 class Serve < Formula
   desc "serve"
   homepage "https://github.com/servehub/serve"
-  version "1.6.146"
+  version "1.6.174"
 
-  url "https://github.com/servehub/serve/releases/download/v1.6.146/serve-v1.6.146-darwin-amd64"
-  sha256 "2d3325eb54bd8b632c19467c78505c2e2a59f8f7b31792973a1d0914298856e9"
+  url "https://github.com/servehub/serve/releases/download/v1.6.174/serve-v1.6.174-darwin-amd64"
+  sha256 "f11496f4e5f404920dbc9ea99ef99d6e7ecbc54b96ee9839e33be975635d51f3"
 
   def install
-    bin.install "serve-v1.6.146-darwin-amd64" => "serve"
+    bin.install "serve-v1.6.174-darwin-amd64" => "serve"
   end
 
   test do
